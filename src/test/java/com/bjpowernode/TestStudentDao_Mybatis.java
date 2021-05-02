c09c428e7a17735dca090344814071257da6f6aa
zhongxia <749807573@qq.com>
2021-05-02 17:17:20 +0800

    多表联查返回map

--- src/test/java/com/bjpowernode/TestStudentDao_Mybatis.java
+++ src/test/java/com/bjpowernode/TestStudentDao_Mybatis.java
@@ -1,16 +1,28 @@
 package com.bjpowernode;
-
+import com.bjpowernode.Dao.StudentsDao;
 import com.bjpowernode.domain.Students;
 import com.bjpowernode.util.MybatisUtil;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class TestStudentDao_Mybatis {
     @Test
     public void TestSelectDouble(){
         SqlSession sqlSession = MybatisUtil.getSqlSession();
-        sqlSession.getMapper(SudentsDao.class);
+        StudentsDao dao = sqlSession.getMapper(StudentsDao.class);
+        List<Map<String,Object>> list= dao.selectDouble();
+        for (Map map:list){
+            Set<String> keySet = map.keySet();
+            for (String key:keySet){
+                Object value = map.get(key);
+                System.out.println(key+"="+value);
+            }
+
+
+        }
     }
 }
