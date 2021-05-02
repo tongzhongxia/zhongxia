2ab3a573281d734fb8d0f757336d7f8d5df9af5c
zhongxia <749807573@qq.com>
2021-05-02 17:05:58 +0800

    123

--- src/main/java/com/bjpowernode/Dao/StudentsDao.java
+++ src/main/java/com/bjpowernode/Dao/StudentsDao.java
@@ -3,8 +3,10 @@ package com.bjpowernode.Dao;
 import com.bjpowernode.domain.Students;
 
 import java.util.List;
+import java.util.Map;
 
 public interface StudentsDao {
     List<Students> selectStudents();
     Students selectById(Students student);
+    List<Map<String,Object>> selectDouble();
 }
